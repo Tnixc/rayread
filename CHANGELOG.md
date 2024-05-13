7893ae6d922153fedec08c33cad461a11a36dd55
Tnixc <tnixxc@gmail.com>
2024-05-13 13:54:47 -0400

    things

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# Hello World Changelog
+
+## [Initial Version] - 2024-05-13
---
+++ src/clipboard-reader.ts
@@ -0,0 +1,26 @@
+import { showHUD, Clipboard, open, getPreferenceValues } from "@raycast/api";
+import { writeFile } from "fs/promises";
+import { join } from "path";
+
+export default async function main() {
+  const DATA = await Clipboard.readText();
+  const CSS_URL: string = getPreferenceValues().css;
+  if (!DATA) {
+    await showHUD("No data in clipboard");
+    return;
+  }
+  const FILE = join("tmp", "raycast_reader.html");
+  const TEMPLATE = `
+    <head>
+    <title>Raycast Reader</title>
+      <link rel="stylesheet" type="text/css" href="${CSS_URL}" />
+    </head>
+    <body>
+    <article id="container">
+    ${DATA}
+    </article>
+    </body>
+    `;
+  await writeFile(FILE, TEMPLATE);
+  await open(FILE);
+}
